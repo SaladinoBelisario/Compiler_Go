e21de39a3c8abb52b3934a79ddefe540913107fa
Saladino Belisario <SaladinoBelisario@protonmail.ch>
2021-05-25 17:13:13 -0500

    String Support
    String Sopport added for the Monkey Language.

--- src/monkey/compiler/compiler.go
+++ src/monkey/compiler/compiler.go
@@ -177,43 +177,6 @@ func (c *Compiler) Compile(node ast.Node) error {
 	case *ast.StringLiteral:
 		str := &object.String{Value: node.Value}
 		c.emit(code.OpConstant, c.addConstant(str))
-	case *ast.ArrayLiteral:
-		for _, el := range node.Elements {
-			err := c.Compile(el)
-			if err != nil {
-				return err
-			}
-		}
-		c.emit(code.OpArray, len(node.Elements))
-	case *ast.HashLiteral:
-		var keys []ast.Expression
-		for k := range node.Pairs {
-			keys = append(keys, k)
-		}
-		sort.Slice(keys, func(i, j int) bool {
-			return keys[i].String() < keys[j].String()
-		})
-		for _, k := range keys {
-			err := c.Compile(k)
-			if err != nil {
-				return err
-			}
-			err = c.Compile(node.Pairs[k])
-			if err != nil {
-				return err
-			}
-		}
-		c.emit(code.OpHash, len(node.Pairs)*2)
-	case *ast.IndexExpression:
-		err := c.Compile(node.Left)
-		if err != nil {
-			return err
-		}
-		err = c.Compile(node.Index)
-		if err != nil {
-			return err
-		}
-		c.emit(code.OpIndex)
 	}
 	return nil
 }
