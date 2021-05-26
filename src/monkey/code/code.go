c4918e39b52b7448c03f8c4254130408fc16c286
Saladino Belisario <SaladinoBelisario@protonmail.ch>
2021-05-25 19:42:58 -0500

    Functions 1
    Functions and Literals

--- src/monkey/code/code.go
+++ src/monkey/code/code.go
@@ -35,6 +35,9 @@ const (
 	OpArray
 	OpHash
 	OpIndex
+	OpCall
+	OpReturnValue
+	OpReturn
 )
 
 var definitions = map[Opcode]*Definition{
@@ -59,6 +62,9 @@ var definitions = map[Opcode]*Definition{
 	OpArray: {"OpArray", []int{2}},
 	OpHash: {"OpHash", []int{2}},
 	OpIndex: {"OpIndex", []int{}},
+	OpCall: {"OpCall", []int{}},
+	OpReturnValue: {"OpReturnValue", []int{}},
+	OpReturn: {"OpReturn", []int{}},
 }
 
 func Lookup(op byte) (*Definition, error) {
